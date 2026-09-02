2ca3d6745aa21c97e0cceb3e78bf7149f038c87b
Tom Spencer <80737736+swonky@users.noreply.github.com>
2026-09-02 21:01:13 +1000

    fix: regenerated parser for release

--- src/parser.c
+++ src/parser.c
@@ -66130,7 +66130,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_caddyfile(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 1,
+      .minor_version = 2,
       .patch_version = 0,
     },
   };
