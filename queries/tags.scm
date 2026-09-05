bec6a72dad4a6c6056c781537e0ada32c8bf60b8
Tom Spencer <80737736+swonky@users.noreply.github.com>
2026-09-05 13:41:18 +1000

    refactor: tidied up tag queries

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,15 +1,17 @@
+; Named matchers
 (named_matcher_definition name: (_) @definition.matcher)
 (named_matcher_reference name: (_) @reference.matcher)
 
+; Snippets
 (snippet_declaration name: (_) @definition.snippet)
-
-(named_route_declaration name: (_) @definition.route)
-
-(invoke_statement route: (_) @reference.route)
 (import_statement pattern: (identifier) @reference.snippet)
 
-(variable_declaration (assignment key: (_) @definition.variable))
+; Named routes
+(named_route_declaration name: (_) @definition.route)
+(invoke_statement route: (_) @reference.route)
 
+; Variables
+(variable_declaration (assignment key: (_) @definition.variable))
 (
 	(generic_placeholder 
 		(namespace_expression 
@@ -19,3 +21,6 @@
 	)
 	(#match? @module-name "vars")
 )
+
+; Directives
+(directive name: (_) @reference.directive))
